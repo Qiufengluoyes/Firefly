048628cb271426b03208bc9324cc0e0e15511e44
Qiufengluoyes <qiufengluoyes@outlook.com>
2025-11-30 10:50:09 +0800

    feat(config): 启用桌面和移动端横幅图片来源文本显示

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -128,8 +128,8 @@ export const siteConfig: SiteConfig = {
       },
       credit: {
         enable: {
-          desktop: false, // 桌面端显示横幅图片来源文本
-          mobile: false, // 移动端显示横幅图片来源文本
+          desktop: true, // 桌面端显示横幅图片来源文本
+          mobile: true, // 移动端显示横幅图片来源文本
         },
         text: {
           desktop: "Steam - 冬季", // 桌面端要显示的来源文本
