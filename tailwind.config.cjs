a466581f957f9551ffb2a64bd6b7e2e322800544
Qiufengluoyes <qiufengluoyes@outlook.com>
2025-12-02 23:09:14 +0800

    feat: 更新字体配置，禁用自定义字体功能并更改所选字体；在布局中添加字体样式表链接；优化 Tailwind CSS 配置格式

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
-  theme: {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+	darkMode: "class", // allows toggling dark mode manually
+	theme: {
 		extend: {
 			fontFamily: {
 				sans: [
-          "FZLanTingYuanS Bold",
-          "FZLanTingYuanS DemiBold",
+					"FZLanTingYuanS DemiBold",
+					"FZLanTingYuanS Bold",
 					...defaultTheme.fontFamily.sans,
 				],
 			},
-      codeFontFamily: {
-        sans: [
-          "JetBrains Mono Variable",
-          ...defaultTheme.fontFamily.sans,
-        ]
-      },
+            codeFontFamily: {
+                sans: [
+                    "JetBrains Mono Variable",
+                    ...defaultTheme.fontFamily.sans,
+                ]
+            },
 		},
 	},
-  plugins: [
-    require("@tailwindcss/typography"),
-  ],
-}
+	plugins: [require("@tailwindcss/typography")],
+};
