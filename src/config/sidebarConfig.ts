ee048db65be643080e494193fd95539fa9fb69b8
Qiufengluoyes <qiufengluoyes@outlook.com>
2026-01-02 10:07:54 +0800

    fix(sidebarConfig): 更新侧边栏组件配置以优化显示和响应式行为

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -85,7 +85,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：广告栏组件 1
 			type: "advertisement",
 			// 是否启用该组件
-			enable: true,
+			enable: false,
 			// 组件显示顺序
 			order: 5,
 			// 组件位置
