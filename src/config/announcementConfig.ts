3ff4c6755393c3adb6845dea0863f16cdf68c1f7
Qiufengluoyes <qiufengluoyes@outlook.com>
2026-02-23 12:03:11 +0800

    feat: 更新公告标题和内容以反映当前状态

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,10 +2,10 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
 	// 公告标题
-	title: "公告",
+	title: "您所寻找的用户没有失联",
 
 	// 公告内容
-	content: "2026 新年快乐！",
+	content: "我还在慢慢完善网站呢，只是文章要等段时间更新了……",
 
 	// 是否允许用户关闭公告
 	closable: false,
